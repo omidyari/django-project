b77ad31f239becfbe1ab9212b140130f91878cb4
omidyari <Omidyari154@gmail.com>
2022-01-23 23:50:32 +0330

    made my blog post and contact model

--- Napp/models.py
+++ Napp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
-    title = models.CharField(max_length=250)
-    text = models.TextField()
+class contact(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField()
+    subject = models.CharField(max_length=250)
+    massage = models.TextField()
+    creted_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    creted_date = models.DateField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(null=True)
