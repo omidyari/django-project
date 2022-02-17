9d7777087b4c0d33408d681a4e53377fba285ddb
omidyari <Omidyari154@gmail.com>
2022-02-17 13:59:09 +0330

    made my model
    made my model and catgory

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import post
+from .models import post, category
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'creted_date'
     empty_value_display = '-empty-'
     #fields = ('title',)
     list_display = ('title', 'status', 'counted_views')
 admin.site.register(post, PostAdmin)
+admin.site.register(category)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
 class post(models.Model):
+    image = models.ImageField(upload_to='blog/', default='blog/defult.jpg')
+    escriber = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     title = models.CharField(max_length=250)
     text = models.TextField()
+    category = models.ManyToManyField(category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     creted_date = models.DateField(auto_now_add=True)
